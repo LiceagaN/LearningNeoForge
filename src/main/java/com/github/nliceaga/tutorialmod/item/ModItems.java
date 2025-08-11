67fe4b15911bf0d425dec73d9a8c9596c279f6c8
Noe Liceaga <nliceagar@gmail.com>
2025-08-10 20:56:35 -0600

    Blocks and Items, basic

---
+++ src/main/java/com/github/nliceaga/tutorialmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.github.nliceaga.tutorialmod.item;
+
+import com.github.nliceaga.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TutorialMod.MOD_ID);
+
+    public static final DeferredItem<Item> BISMUT = ITEMS.register("bismuth",
+            () -> new Item(new Item.Properties()));
+    public static final  DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
+            () -> new Item(new Item.Properties()));
+
+    public static void  register(IEventBus bus) {
+        ITEMS.register(bus);
+    }
+
+}
+
